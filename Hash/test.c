4f99519204557fa770ce737a8d35d52f94630ec9
Master52 <harajmast9008@gmail.com>
2018-12-03 10:31:00 +0530

    removed Hash

--- Hash/test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "Hash.c"
-int main(int argc,char* argv[]) {
-	HashMap *foo = Hash_create(sizeof(int),NULL,NULL);
-	int a = 10;
-	Hash_add(foo,"Haitm",&a);
-	return 0;
-} 
